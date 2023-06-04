b35556ab38466cc53c674a77ad4f464f3dac7223
Prashant-2505 <shramaprashant2505@gmail.com>
2023-06-04 12:08:54 +0530

    navbar scroling feature done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,8 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <main >
-     <p className="text-red-800">'use client'</p>
+    <main className="h-[200vh]">
+     <p className="text-red-800 ">'use client'</p>
     </main>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,42 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Wrapper from './Wrapper';
 import Link from 'next/link';
 import Menu from './Menu';
+import MenuMobile from './MenuMobile';
+
+import { IoMdHeartEmpty } from 'react-icons/io'
+import { BsCart } from "react-icons/bs"
+import { BiMenuAltRight } from "react-icons/bi"
+import { VscChromeClose } from "react-icons/vsc"
 
 const Header = () => {
   const [showCatMenu, setShowCatMenu] = useState(false);
   const [show, setShow] = useState('translate-y-0');
   const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY) {
+        setShow('-translate-y-[90px]')
+      }
+      else {
+        setShow("translate-y-0 shadow-lg")
+      }
+    }
+    else {
+      setShow('translate-y-0')
+    }
+    setLastScrollY(window.scrollY)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar)
+    return () => {
+      window.removeEventListener("scroll", controlNavbar)
+    }
+  }, [lastScrollY])
 
   return (
     <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transfrom duration-300 ${show}`}>
@@ -15,8 +44,41 @@ const Header = () => {
         <Link href='/'>
           <p className='w-[40px] md:w-[60px]'>Sneaker</p>
         </Link>
-        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
 
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+        {mobileMenu && (<MenuMobile showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} setMobileMenu={setMobileMenu} />)}
+
+        <div className='flex items-center gap-2 text-black'>
+          {/* icon start */}
+          <div className='w-8 md:w-12 h-8 md:h-12 rounded-full justify-center flex items-center hover:bg-black/[0.05] cursor-pointer relative'>
+            <IoMdHeartEmpty className='text-[19px] md:text-[24px]' />
+            <div className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-cneter items-center px-[2px] md:px-[5px]'>15</div>
+          </div>
+          {/* icon end */}
+
+          {/* icon start */}
+          <div className='w-8 md:w-12 h-8 md:h-12 rounded-full justify-center flex items-center hover:bg-black/[0.05] cursor-pointer relative'>
+            <BsCart className='text-[15px] md:text-[20px]' />
+            <div className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-cneter items-center px-[2px] md:px-[5px]'>5</div>
+          </div>
+          {/* icon end */}
+
+          {/* mobile icon start */}
+          <div className='flex  w-8 md:w-12 h-8 md:h-12 rounded-full md:hidden justify-center items-center hover:bg-black/[0.05] cursor-ponter relative -mr-2'>
+            {mobileMenu ?
+              (
+                <VscChromeClose className='text-[16px]'
+                  onClick={() => setMobileMenu(false)}
+                />
+              )
+              :
+              (
+                <BiMenuAltRight className='text-[20px]'
+                  onClick={() => setMobileMenu(true)}
+                />
+              )}
+          </div>
+        </div>
       </Wrapper>
     </header>
   );
